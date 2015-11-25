645d5d8f716e7f41f0207180c673c530544031ee
Chris Warburton <chriswarbo@gmail.com>
2015-11-25 00:55:40 +0000

    Added test script

---
+++ test.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p bash pandoc
+
+MARKDOWN="*foo*"
+echo "Got Markdown '$MARKDOWN'"
+
+JSON=$(echo "$MARKDOWN" | pandoc -f markdown -t json)
+echo "Got JSON '$JSON'"
+
+UNWRAP=$(echo '```{.unwrap}'; echo "$JSON"; echo '```')
+echo "Got unwrap '$UNWRAP'"
+
+META=$(echo "$UNWRAP" | pandoc -f markdown -t json)
+echo "Got meta '$META'"
+
+UNWRAP=$(echo "$META" | cabal run -v0) || {
+    echo "Failed to unwrap" >> /dev/stderr
+    exit 1
+}
+echo "Got unwrapped '$UNWRAP'"
+
+FINAL=$(echo "$UNWRAP" | pandoc -f json -t html)
+echo "Got HTML '$FINAL'"
+
+echo "$FINAL" | grep '<em>foo</em>' || {
+    echo "Didn't unwrap *foo* in '$FINAL'" >> /dev/stderr
+    exit 1
+}
