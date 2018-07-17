68db12a6b4ec616918c7e4d7c55a3ade52dad621
Chris Warburton <chriswarbo@gmail.com>
2018-07-17 03:20:03 +0100

    Use haskellRelease

--- release.nix
+++ release.nix
@@ -3,45 +3,19 @@
 # Provides a bunch of nixpkgs versions, augmented with useful helper functions
 with builtins;
 with rec {
-  pinnedConfig = (import <nixpkgs> { config = {}; }).fetchgit {
-    url    = http://chriswarbo.net/git/nix-config.git;
-    rev    = "7b96c3a";
-    sha256 = "0sqhf599ll2qshbnmspni5r3h5mfwbbkyz5xhfb3jxhflba594zw";
-  };
-
-  pkgSets = { pinnedConfig = import pinnedConfig {}; } //
-    (with tryEval <nix-config>;
-     if success
-        then { pathConfig = import value {}; }
-        else {});
-
+  pkgs    = import <nixpkgs> {};
+  helpers = pkgs.nix-helpers or import (pkgs.fetchgit {
+    url    = http://chriswarbo.net/git/nix-helpers.git;
+    rev    = "6227a40";
+    sha256 = "077kcal167ixwjj41sqrndd5pwvyavs20h826qx3ijl2i02wmwxs";
+  });
 };
-
-with pkgSets.pinnedConfig.nixpkgs1709.lib;
-with rec {
-  skipBroken = f: name: if hasPrefix "ghcjs" name || hasPrefix "lts" name
-    then (_: null)
-    else f;
-
-  # "self" is a customised nixpkgs set, "super" is the corresponding original
-  buildForNixpkgs = self: super: mapAttrs (skipBroken (buildForHaskell self))
-                                          super.haskell.packages;
-
-  buildForHaskell = pkgs: hsPkgs: rec {
-    # Uses Haskell package set provided by nixpkgs
-    nixpkgsExpr  = pkgs.runCabal2nix { url = ./.; };   # Useful GC root
-    nixpkgsDeps  = hsPkgs.callPackage nixpkgsExpr {};  # Actual Haskell build
-
-    # Uses a Cabal sandbox to pick dependencies from (a snapshot of) Hackage
-    hackageDeps = pkgs.haskellPkgWithDeps {
-      inherit hsPkgs;
-      delay-failure = true;
-      dir           = ./.;
-    };
+with pkgs // helpers // pkgs.lib;
+collapseAttrs (haskellRelease {
+  name        = "panhandle";
+  dir         = ./.;
+  hackageSets = {
+    nixpkgs1709 = [ "ghc7103" ];
+    nixpkgs1803 = [ "ghc7103" ];
   };
-
-  buildForPkgSet = pkgSet:
-    mapAttrs (name: self: buildForNixpkgs self (getAttr name pkgSet))
-             pkgSet.customised;
-};
-mapAttrs (_: buildForPkgSet) pkgSets
+})
