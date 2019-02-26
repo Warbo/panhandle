3e57fd816bd3616ce68912937f394be63f3e0bac
Chris Warburton <chriswarbo@gmail.com>
2019-02-26 18:13:36 +0000

    Run tests from release.nix

--- release.nix
+++ release.nix
@@ -1,4 +1,5 @@
 # Used for building and testing on build servers like Hydra
+with builtins;
 with import ./nixpkgs.nix;
 with lib;
 with {
@@ -8,4 +9,31 @@ with {
 {
   "nixpkgs${nixpkgsVersion}-ghc${ghcVersion}-panhandle" =
     haskellPackages.panhandle;
+
+  "tests" = runCommand "panhandle-tests"
+    {
+      buildInputs = [
+        cabal-install
+        haskellPackages.pandoc
+
+        # We include panhandle, since this will guarantee our dependencies
+        (haskellPackages.ghcWithPackages (h: [ h.panhandle ]))
+      ];
+      dir         = filterSource
+        (path: _:
+          with { f = baseNameOf path; };
+          !(hasSuffix ".nix" f || elem f [
+            ".git" ".gitignore" ".issues" "dist" "dist-newstyle"
+            "README.md" "result"
+          ]))
+        ./.;
+    }
+    ''
+      export HOME="$PWD"
+      cp -r "$dir" dir
+      chmod 777 -R dir
+      cd dir
+      ./test.sh
+      echo pass > "$out"
+    '';
 }
