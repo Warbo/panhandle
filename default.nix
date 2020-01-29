8985641f447a6caba93d6520e24a26550e7976c3
Chris Warburton <chriswarbo@gmail.com>
2020-01-29 01:17:24 +0000

    Fix Nix packaging

--- default.nix
+++ default.nix
@@ -20,4 +20,59 @@ pkgs.haskell-nix.cabalProject {
   src         = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
   ghc         = pkgs.buildPackages.pkgs.haskell-nix.compiler.ghc865;
   index-state = "2020-01-11T00:00:00Z";
+
+  # Avoid 'Neither the Haskell package set or the Nixpkgs package set contain
+  # the package: alex (build tool dependency).'
+  # The issue AFAIK seems to be that "bootstrap" packages (used by GHC) can
+  # either be fixed, to avoid excessive rebuilding; or reinstallable, to allow
+  # overrides, etc. There are some packages which are the wrong way around
+  # somewhere in the dependencies of lazysmallcheck2012 (not sure which, maybe
+  # terminfo), so we specify them explicitly here. Found this via trial and
+  # error, thanks to https://github.com/input-output-hk/haskell.nix/issues/221
+  modules = [
+    { reinstallableLibGhc = true; }
+    {
+      nonReinstallablePkgs = [
+        "array"
+        "array"
+        "base"
+        "binary"
+        "bytestring"
+        "Cabal"
+        "containers"
+        "deepseq"
+        "directory"
+        "filepath"
+        "ghc"
+        "ghc-boot"
+        "ghc-boot"
+        "ghc-boot-th"
+        "ghc-compact"
+        "ghc-heap"
+        "ghc-prim"
+        "ghc-prim"
+        "ghci"
+        "ghcjs-prim"
+        "ghcjs-th"
+        "haskeline"
+        "hpc"
+        "integer-gmp"
+        "integer-simple"
+        "mtl"
+        "parsec"
+        "pretty"
+        "process"
+        "rts"
+        "stm"
+        "template-haskell"
+        "terminfo"
+        "text"
+        "time"
+        "transformers"
+        "unix"
+        "Win32"
+        "xhtml"
+      ];
+    }
+  ];
 }
--- release.nix
+++ release.nix
@@ -1,11 +1,7 @@
 # Used for building and testing on build servers like Hydra
 with import ./.;
-panhandle.components.exes // {
-  inherit (panhandle.components) library;
-  tests = {
-    # FIXME: error: attribute 'configFiles' missing, at /nix/store/5sl7sk4pq1di6ainl5p8rzs98rn6q24v-haskell.nix/builder/make-config-files.nix:75:16
-    #unit        = panhandle.components.tests.tests;
-
+panhandle.components // {
+  tests = panhandle.components.tests // {
     integration = (import <nixpkgs> {}).runCommand "panhandle-integration-tests"
       {
         buildInputs = [
