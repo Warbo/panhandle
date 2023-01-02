4a10de6e546315cd692475d34a24262ed542ece5
Chris Warburton <chriswarbo@gmail.com>
2023-01-02 00:08:43 +0000

    Avoid haskell-nix

--- release.nix
+++ release.nix
@@ -1,17 +1,16 @@
 # Used for building and testing on build servers like Hydra
-with import ./.;
-panhandle.components // {
-  tests = panhandle.components.tests // {
-    integration = (import <nixpkgs> {}).runCommand "panhandle-integration-tests"
-      {
-        buildInputs = [
-          pandoc.components.exes.pandoc
-          panhandle.components.exes.panhandle
-        ];
-      }
-      ''
-        bash "${./test.sh}"
-        echo pass > "$out"
-      '';
+{
+  nixpkgs ? import (fetchTarball {
+    name   = "nixpkgs1909";
+    url    = https://github.com/NixOS/nixpkgs/archive/19.09.tar.gz;
+    sha256 = "0mhqhq21y5vrr1f30qd2bvydv4bbbslvyzclhw0kdxmkgg3z4c92";
+  }) {}
+}:
+with {
+  pkgs = nixpkgs.haskellPackages.override {
+    overrides = self: super: {
+      panhandle = self.callPackage (nixpkgs.callPackage ./. {}) {};
+    };
   };
-}
+};
+pkgs.panhandle
