7b207ea0513552bdd78cbd6efcd1cb82e8d4dd64
Chris Warburton <chriswarbo@gmail.com>
2023-09-18 13:21:44 +0100

    Make default.nix self-contained

--- default.nix
+++ default.nix
@@ -1,4 +1,26 @@
-{ haskellPackages, lib }: haskellPackages.haskellSrc2nix {
-  name = "panhandle";
-  src = lib.cleanSource ./.;
-}
+{ nixpkgs ? import (fetchTarball {
+  name = "nixpkgs2305";
+  url = "https://github.com/NixOS/nixpkgs/archive/23.05.tar.gz";
+  sha256 = "10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf";
+}) {
+  config = { };
+  overlays = [ ];
+}, haskellPackages ? nixpkgs.haskellPackages, lib ? nixpkgs.lib }:
+with rec {
+  inherit (lib) functionArgs setFunctionArgs;
+
+  # Raw cabal2nix result (a function returning a derivation)
+  raw = haskellPackages.callCabal2nix "panhandle" (lib.cleanSource ./.);
+
+  # Wrapper around raw, which patches the resulting derivation
+  fixed = (args:
+    (raw args).overrideAttrs
+    (old: { buildInputs = old.buildInputs ++ [ nixpkgs.libopcodes ]; }) // {
+      # Include these in the result, in case we want to override
+      inherit build raw;
+    });
+
+  # Same as fixed, but inherits the function metadata of raw
+  build = setFunctionArgs fixed raw;
+};
+build { }
